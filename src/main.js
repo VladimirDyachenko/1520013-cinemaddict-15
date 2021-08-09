ac9346720d5d5bc434cda3bb5bfcd39fe79d86ca
VladimirDyachenko <d.vovan3@gmail.com>
2021-08-09 16:53:34 +0300

    Добавил компоненты главной страницы
    Компоненты главной страницы в папке films.
    Временно компонент films-list-extra содержит карточки с фильмами, в дальнейшем уберу их и буду использовать film-card.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import { getSiteMenuTemplate } from './view/site-menu.js';
 import { getFooterStatisticsTemplate } from './view/footer-statistic.js';
 import { getStatisticsTemplate } from './view/statistic/statistic.js';
 import { getUserProfileTemplate } from './view/user-profile.js';
+import { getFilmsListTemplate } from './view/films/films-list.js';
+import { getSortTemplate } from './view/films/sort.js';
+import { getFilmCardTemplate } from './view/films/film-card.js';
+import { getShowMoreButtonTemplate } from './view/films/show-more-button.js';
+import { getFilmsListExtraTemplate } from './view/films/films-list-extra.js';
+import { getFilmModalTemplate } from './view/films/film-modal.js';
+import { getCommentTemplate } from './view/films/comment.js';
+
 const InsertPosition = {
   AFTER_BEGIN: 'afterbegin',
   AFTER_END: 'afterend',
@@ -24,7 +32,54 @@ const renderStatsPage = () => {
   render(footerElement, getFooterStatisticsTemplate(), InsertPosition.BEFORE_END);
 };
 
+const renderFilmModal = () => {
+  //Details modal
+  render(document.body, getFilmModalTemplate(), InsertPosition.BEFORE_END);
+  const commentsListElement = document.querySelector('.film-details__comments-list');
+
+  //Comments
+  render(commentsListElement, getCommentTemplate(), InsertPosition.AFTER_BEGIN);
+  render(commentsListElement, getCommentTemplate(), InsertPosition.AFTER_BEGIN);
+  render(commentsListElement, getCommentTemplate(), InsertPosition.AFTER_BEGIN);
+  render(commentsListElement, getCommentTemplate(), InsertPosition.AFTER_BEGIN);
+};
+
 const renderMainPage = () => {
+  //User profile
+  render(headerElement, getUserProfileTemplate(), InsertPosition.BEFORE_END);
+
+  //Site nav
+  render(mainElement, getSiteMenuTemplate(), InsertPosition.AFTER_BEGIN);
+
+  //Sort list
+  render(mainElement, getSortTemplate(), InsertPosition.BEFORE_END);
+
+  //Films container
+  const filmsList = document.createElement('div');
+  render(filmsList, getFilmsListTemplate(), InsertPosition.AFTER_BEGIN);
+
+  const filmsContainerElement = filmsList.querySelector('.films');
+
+  const filmsListContainerElement = filmsContainerElement.querySelector('.films-list__container');
+  render(filmsListContainerElement, getFilmCardTemplate(), InsertPosition.AFTER_BEGIN);
+  render(filmsListContainerElement, getFilmCardTemplate(), InsertPosition.AFTER_BEGIN);
+  render(filmsListContainerElement, getFilmCardTemplate(), InsertPosition.AFTER_BEGIN);
+  render(filmsListContainerElement, getFilmCardTemplate(), InsertPosition.AFTER_BEGIN);
+  render(filmsListContainerElement, getFilmCardTemplate(), InsertPosition.AFTER_BEGIN);
+
+  const filmsListElement = filmsContainerElement.querySelector('.films-list');
+  render(filmsListElement, getShowMoreButtonTemplate(), InsertPosition.BEFORE_END);
+
+  //Extra lists
+  render(filmsContainerElement, getFilmsListExtraTemplate(), InsertPosition.BEFORE_END);
+  render(filmsContainerElement, getFilmsListExtraTemplate(), InsertPosition.BEFORE_END);
+
+  mainElement.appendChild(filmsContainerElement);
+
+  //Footer statistics
+  render(footerElement, getFooterStatisticsTemplate(), InsertPosition.BEFORE_END);
+
+  renderFilmModal();
 };
 
 const setPage = (isStatPage = false) => isStatPage ? renderStatsPage() : renderMainPage();
