3862957a4dc55ffdbc2582e81dbed6bbb75c2a1f
VladimirDyachenko <d.vovan3@gmail.com>
2021-08-10 21:17:59 +0300

    Удалил лишний пробел

--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -4,7 +4,7 @@ export const getSiteMenuTemplate = (filterData) => (`
     <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
     <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${filterData.watchList.length}</span></a>
     <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${filterData.historyList.length}</span></a>
-    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${filterData.favoriteList .length}</span></a>
+    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${filterData.favoriteList.length}</span></a>
   </div>
   <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>
